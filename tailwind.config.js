5bfe9bd0332d8648ab05ac83ec169fb230d20bb4
shanks-Inc <shanksandassociatesinc@gmail.com>
2025-08-19 00:56:05 -0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        'brand-black': '#0a0a0a',
+        'brand-burgundy': '#7a1f2b',
+        gold: '#D4AF37',
+      },
+    },
+  },
+  plugins: [],
+}
